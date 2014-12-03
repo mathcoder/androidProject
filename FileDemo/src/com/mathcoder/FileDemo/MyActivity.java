890bad53a3e053dbe81e648fd0abb502b3f95ce4
mathOfCrazy <1249818205@qq.com>
2014-12-03 21:02:40 +0800

    --push有误。。

---
+++ FileDemo/src/com/mathcoder/FileDemo/MyActivity.java
@@ -0,0 +1,26 @@
+package com.mathcoder.FileDemo;
+
+import Utils.FileUtil;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+
+
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        FileUtil fHelper = new FileUtil(getApplicationContext());
+        fHelper.saveFile(fHelper.getTargetPath(FileUtil.PATH_SDCARD_ANDROID_PACKBAGE_CACHE), "123.html", "<html>Hello World</html>");
+        super.onWindowFocusChanged(hasFocus);
+    }
+}
